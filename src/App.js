95777ef84553809419cb76b31b2b1c80319caaae
Sean Graves <seankgraves@gmail.com>
2019-10-26 18:58:03 -0400

    mobx decorators working!

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SideBar from './sidebar/Sidebar';
 import './App.css';
 import styled from 'styled-components';
 import Header from './header/Header';
+import { observable, computed } from 'mobx';
 import { observer } from 'mobx-react';
 
 import markers from './Markers';
@@ -15,8 +16,19 @@ const Wrapper = styled.div`
   height: calc(100vh - ${headerHeight});
 `;
 
+class TodoList {
+  @observable todos = []
+  @computed get unfinishedTodoCount() {
+      return this.todos.filter(todo => !todo.finished).length
+  }
+}
 
+@observer
 class App extends Component {
+
+  constructor(){
+    super();
+  }
 
   render(){
     return (<>
