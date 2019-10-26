8f94ecebdd4b1895f2c9f44f94691decda622f77
Sean Graves <seankgraves@gmail.com>
2019-10-26 16:22:05 -0400

    Basic styling for list items

--- src/sidebar/Item.js
+++ src/sidebar/Item.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import {GiPoliceOfficerHead} from 'react-icons/gi';
 
 const ItemWrapper = styled.div`
     padding: 5px;
+    display: flex;
+    border-bottom: 1px solid #eee;
+    cursor: pointer;
+`;
+
+const Icon = styled(GiPoliceOfficerHead)`
+`;
+
+const Content = styled.div`
+    padding-left: 10px;
 `;
 
 class Item extends Component {
 
   render() {
-    const {id} = this.props;
+    const {id, status, user} = this.props;
     return (
-        <ItemWrapper>{id}</ItemWrapper>
+        <ItemWrapper>
+            <Icon size="30" color={status} />
+            <Content>
+                <div>User ID: {user.id}</div>
+                <div>Phone Number: {user.phone}</div>
+            </Content>
+        </ItemWrapper>
     );
   }
 }
