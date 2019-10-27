534d3969b519222b7dbc58932622928b3e1be302
Sean Graves <seankgraves@gmail.com>
2019-10-26 22:32:20 -0400

    Got more dummy data from jeesp

--- src/Store.js
+++ src/Store.js
@@ -1,8 +1,22 @@
 import { observable } from 'mobx';
-import markers from './Markers';
+// import markers from './Markers';
+
+import markers from './markers.json';
+
+console.log(markers);
+
+
+const cleant = markers.map( m => ({
+    ...m,
+    id: m.name,
+    user: {
+        id: m.lat,
+        phone: m.lng
+    }
+}));
 
 class Store {
-  @observable markers = markers;
+  @observable markers = cleant;
   @observable selected = null;
   @observable hover = null;
 }
